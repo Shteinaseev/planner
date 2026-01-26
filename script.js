c443580a36bc218594f72156e9ffd5e22d429000
unknown <shteinaseev@gmail.com>
2026-01-26 11:47:43 +0100

    fixed styles

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 class Planner {
     selectors = {
         panel: '[data-js-panel]',
-        day: '[data-js-panel-day]',
-        time: '[data-js-panel-time]'
+        dateEl: '[data-js-panel-date]',
+        clockEl: '[data-js-panel-clock]'
     }
 
     days = ['Nedelja', 'Ponedeljak', 'Utorak', 'Sreda', 'Četvrtak', 'Petak', 'Subota'];
 
-
     constructor() {
         this.panel = document.querySelector(this.selectors.panel)
-        this.day = this.panel.querySelector(this.selectors.day)
-        this.time = this.panel.querySelector(this.selectors.time)
-        this.getTime()
+        this.dateEl = this.panel.querySelector(this.selectors.dateEl)
+        this.clockEl = this.panel.querySelector(this.selectors.clockEl)
+        this.showTime()
+        setInterval(this.showTime.bind(this), 1000);
+
     }
 
-    getTime() {
-        const date = new Date()
-        const dayName = this.days[date.getDay()];
-        this.day.innerHTML = dayName + ' ' + date.toLocaleDateString()
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
-        this.time.innerHTML = hours + ':' + minutes + ':' + seconds
+    showTime() {
+        this.updateClock()
+
+        this.updateDate()
+    }
+
+    updateClock() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+
+        this.clockEl.textContent = `${hours}:${minutes}:${seconds}`;
+    }
+
+    updateDate() {
+        const now = new Date();
+        this.dateEl.textContent =
+            String(now.getDate()).padStart(2, '0') + '.' +
+            String(now.getMonth() + 1).padStart(2, '0') + '.' +
+            now.getFullYear();
     }
 
 }
